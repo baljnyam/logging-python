db81943893fbc167a1bec5fb176a168cfe0fe38b
baljnyam <baljnyam@gmail.com>
2020-03-24 21:24:53 +1100

    .

--- test.py
+++ test.py
@@ -9,5 +9,20 @@ import logging
 # logging.basicConfig(level=logging.DEBUG)
 # logging.debug('This will get logged')
 
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-logging.warning('This will get logged to a file')
+# Loggin to file
+
+# logging.basicConfig(filename='example.log',level=logging.DEBUG)
+# logging.debug('This message should go to the log file')
+# logging.info('So should this')
+# logging.warning('And this, too')
+
+# assuming loglevel is bound to the string value obtained from the
+# command line argument. Convert to upper case to allow the user to
+# specify --log=DEBUG or --log=debug
+
+# numeric_level = getattr(logging, loglevel.upper(), None)
+# if not isinstance(numeric_level, int):
+#     raise ValueError('Invalid log level: %s' % loglevel)
+# logging.basicConfig(level=numeric_level)
+# logging.basicConfig(filename='example.log', filemode='w', level=logging.DEBUG)
+
