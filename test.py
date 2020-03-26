be155e28404ae639e16bc1202497638140a684c5
baljnyam <baljnyam@gmail.com>
2020-03-26 16:04:25 +1100

    .

--- test.py
+++ test.py
@@ -42,5 +42,44 @@ import logging
 # logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 # logging.warning('is when this event was logged.')
 
+#-----------Configuring logging
 
+# create logger
+# logger = logging.getLogger('simple_example')
+# logger.setLevel(logging.DEBUG)
 
+# # create console handler and set level to debug
+# ch = logging.StreamHandler()
+# ch.setLevel(logging.DEBUG)
+
+# # create formatter
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# # add formatter to ch
+# ch.setFormatter(formatter)
+
+# # add ch to logger
+# logger.addHandler(ch)
+
+# 'application' code
+# logger.debug('debug message')
+# logger.info('info message')
+# logger.warning('warn message')
+# logger.error('error message')
+# logger.critical('critical message')
+
+# ----------- names of the objects
+
+import logging.config
+
+logging.config.fileConfig('logging.conf')
+
+# create logger
+logger = logging.getLogger('simpleExample')
+
+# 'application' code
+logger.debug('debug message')
+logger.info('info message')
+logger.warning('warn message')
+logger.error('error message')
+logger.critical('critical message')
